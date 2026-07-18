c3c04fd2ebf39e886540e0bfb0980acea32cdbca
gsmilagres1-dot <gsmilagres1@gmail.com>
2026-07-18 03:37:56 -0300

    Update galaxia.tsx

--- src/routes/_authenticated/galaxia.tsx
+++ src/routes/_authenticated/galaxia.tsx
@@ -7,7 +7,7 @@ import { toast } from "sonner";
 import { listMyIdentities, generateShipImage } from "@/lib/identities.functions";
 import { getJourneyState, startQuiz, submitQuiz, claimVisa } from "@/lib/intergalactic.functions";
 import { DESTINATIONS, ALL_DESTINATIONS, getAnyDestination, MAX_QUIZ_ATTEMPTS, KIND_LABEL, TELEPORTER_THRESHOLD, TELESCOPE_JIMMY_WATH_THRESHOLD, WORMHOLE_SURPRISE_THRESHOLD, type Destination } from "@/lib/intergalactic";
-import { SHIPS } from "@/lib/alien";
+import { SHIPS, type ShipId } from "@/lib/alien";
 import { DestinationBadge } from "@/components/DestinationBadge";
 import { TeleporterPrize } from "@/components/TeleporterPrize";
 import { WormholePrize } from "@/components/WormholePrize";
